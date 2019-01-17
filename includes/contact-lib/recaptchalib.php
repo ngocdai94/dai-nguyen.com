7f9accaaad701a4f7d829b933a0b6fca2b4e67f8
Dai Nguyen <ngocdai94@gmail.com>
2019-01-17 14:05:48 -0800

    update forms

--- includes/contact-lib/recaptchalib.php
+++ includes/contact-lib/recaptchalib.php
@@ -130,7 +130,8 @@ class ReCaptcha
             $recaptchaResponse->success = true;
         } else {
             $recaptchaResponse->success = false;
-            $recaptchaResponse->errorCodes = $answers [error-codes];
+            $recaptchaResponse->errorCodes = $answers [0];
+            // error-codes
         }
 
         return $recaptchaResponse;
